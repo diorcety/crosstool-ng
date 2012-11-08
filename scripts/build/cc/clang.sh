bda5682d55278cdb958bc2d8c1b9b89a24c135da
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2012-11-09 00:20:22 +0100

    Add llvm companion lib

---
+++ scripts/build/cc/clang.sh
@@ -0,0 +1,48 @@
+# This file adds the function to build the clang C compiler
+# Copyright 2012 Yann Diorcet
+# Licensed under the GPL v2. See COPYING in the root of this package
+
+# Download clang
+do_clang_get() {
+    :
+}
+
+# Extract clang
+do_clang_extract() {
+    :
+}
+
+#------------------------------------------------------------------------------
+# Core clang pass 1
+do_clang_core_pass_1() {
+    :
+}
+
+# Core clang pass 2
+do_clang_core_pass_2() {
+    :
+}
+
+#------------------------------------------------------------------------------
+# Build core clang
+do_clang_core_backend() {
+    :
+}
+
+#------------------------------------------------------------------------------
+# Build complete clang to run on build
+do_clang_for_build() {
+    :
+}
+
+#------------------------------------------------------------------------------
+# Build final clang to run on host
+do_clang_for_host() {
+    :
+}
+
+#------------------------------------------------------------------------------
+# Build the final clang
+do_clang_backend() {
+    :
+}
