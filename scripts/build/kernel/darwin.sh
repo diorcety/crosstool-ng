95d1ce8f61438e0dd414f025446924cb2d7c36c3
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2012-11-13 23:34:21 +0100

    Use relative path for darwin scripts

--- scripts/build/kernel/darwin.sh
+++ scripts/build/kernel/darwin.sh
@@ -17,50 +17,53 @@ do_kernel_get() {
 
 do_kernel_extract() {
     CT_DoStep INFO "Extract kernel headers"
-
-    DIST="${CT_SYSROOT_DIR}/"
+    
     SDK_NAME="${CT_DARWIN_SDK}"  
  
+    CT_Pushd "${CT_SYSROOT_DIR}"
+    
     CT_DoExecLog ALL \
-    mkdir -p "${DIST}/SDKs/${SDK_NAME}"
+    mkdir -p "SDKs/${SDK_NAME}"
  
     CT_DoExecLog ALL \
-    cp -aT "${CT_DARWIN_SDK_PATH}" "${DIST}/SDKs/${SDK_NAME}"
+    cp -aT "${CT_DARWIN_SDK_PATH}" "SDKs/${SDK_NAME}"
     
     CT_DoExecLog ALL \
-    rm -rf "${DIST}/SDKs/${SDK_NAME}/Library/Frameworks"
+    rm -rf "SDKs/${SDK_NAME}/Library/Frameworks"
     
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/SDKs/${SDK_NAME}/System/Library/Frameworks" "${DIST}/SDKs/${SDK_NAME}/Library"
+    ln -sf "../System/Library/Frameworks" "SDKs/${SDK_NAME}/Library"
     
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/SDKs/${SDK_NAME}/Developer" "${DIST}/Developer"
+    ln -sf "SDKs/${SDK_NAME}/Developer" "Developer"
     
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/SDKs/${SDK_NAME}/Library" "${DIST}/Library"
+    ln -sf "SDKs/${SDK_NAME}/Library" "Library"
     
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/SDKs/${SDK_NAME}/System" "${DIST}/System"
+    ln -sf "SDKs/${SDK_NAME}/System" "System"
 
     CT_DoExecLog ALL \
-    rm -rf "${DIST}/usr" "${DIST}/lib"
+    rm -rf "usr" "lib"
 
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/SDKs/${SDK_NAME}/usr" "${DIST}/usr"
+    ln -sf "SDKs/${SDK_NAME}/usr" "usr"
   
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/usr/lib/i686-apple-darwin10" "${DIST}/usr/lib/x86_64-apple-darwin10"
+    ln -sf "i686-apple-darwin10" "usr/lib/x86_64-apple-darwin10"
   
-    mkdir -p "${DIST}/usr/lib/gcc/" 
+    mkdir -p "usr/lib/gcc/" 
    
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/usr/lib/gcc/i686-apple-darwin10" "${DIST}/usr/lib/gcc/x86_64-apple-darwin10"
+    ln -sf "i686-apple-darwin10" "usr/lib/gcc/x86_64-apple-darwin10"
     
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/usr/lib" "${DIST}/lib"
+    ln -sf "usr/lib" "lib"
 
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/usr/include" "${DIST}/include"
+    ln -sf "usr/include" "include"
+    
+    CT_Popd
  
     CT_EndStep
 }
@@ -68,8 +71,12 @@ do_kernel_extract() {
 do_kernel_headers() {
     CT_DoStep INFO "Installing kernel headers"
 
+    CT_Pushd "${CT_PREFIX_DIR}/${CT_TARGET}"
+    
     CT_DoExecLog ALL \
-    ln -sf "${DIST}/include" "${CT_PREFIX_DIR}/${CT_TARGET}/include"
+    ln -sf "${CT_SYSROOT_REL_DIR}/include" "include"
+    
+    CT_Popd
 
     CT_EndStep
 }
