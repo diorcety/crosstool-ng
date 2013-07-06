877d65bd762086188f441a79c2da110bdac97819
Ray Donnelly <mingw.android@gmail.com>
2013-07-06 11:14:27 +0100

    Header extraction fixes
    Add c++ headers.
    Fix array looping for system headers.
    Fix CT_DARWIN_MAC_OSX_V_10_6 checking.
    Add arm-apple-darwin11 sample config.

--- scripts/build/kernel/darwin.sh
+++ scripts/build/kernel/darwin.sh
@@ -15,6 +15,7 @@ do_kernel_get() {
     CT_EndStep
 }
 
+# I'm not sure about the usr/ folder prefix. toolchain4 didn't have one finally, but does during the build process.
 do_kernel_extract() {
     CT_DoStep INFO "Extract kernel headers and libraries"
 
@@ -26,13 +27,15 @@ do_kernel_extract() {
 
     declare -a SYSHEADERS
     if [ -z "$CT_ARCH_arm" ] ; then
-        if [ ! -z "CT_DARWIN_MAC_OSX_V_10_6" ] ; then
+        _SDKARCH=i686
+        if [ ! -z "$CT_DARWIN_MAC_OSX_V_10_6" ] ; then
             SYSHEADERS=(libc.h stdio.h errno.h string.h strings.h alloca.h stdlib.h unistd.h time.h dlfcn.h limits.h _types.h _structs.h Availability.h AvailabilityMacros.h AvailabilityInternal.h vproc.h fcntl.h pthread.h pthread_impl.h sched.h sys/select.h sys/unistd.h sys/wait.h sys/errno.h sys/types.h sys/syslimits.h sys/_types.h sys/_endian.h sys/cdefs.h sys/appleapiopts.h sys/_structs.h sys/signal.h sys/resource.h sys/stat.h sys/_select.h sys/fcntl.h machine/types.h machine/endian.h machine/signal.h machine/limits.h machine/_structs.h machine/_limits.h machine/_types.h i386/types.h i386/_types.h i386/endian.h i386/limits.h i386/_limits.h i386/_structs.h i386/signal.h libkern/_OSByteOrder.h libkern/i386/_OSByteOrder.h mach/i386/_structs.h)
         else
             SYSHEADERS=(libc.h stdio.h errno.h string.h strings.h alloca.h stdlib.h unistd.h time.h dlfcn.h limits.h _types.h _structs.h Availability.h AvailabilityMacros.h AvailabilityInternal.h vproc.h fcntl.h pthread.h pthread_impl.h sched.h sys/select.h sys/unistd.h sys/wait.h sys/errno.h sys/types.h sys/syslimits.h sys/_types.h sys/_endian.h sys/cdefs.h sys/appleapiopts.h sys/_structs.h sys/_symbol_aliasing.h sys/_posix_availability.h sys/signal.h sys/resource.h sys/stat.h sys/_select.h sys/fcntl.h machine/types.h machine/endian.h machine/signal.h machine/limits.h machine/_structs.h machine/_limits.h machine/_types.h i386/types.h i386/_types.h i386/endian.h i386/limits.h i386/_limits.h i386/_structs.h i386/signal.h libkern/_OSByteOrder.h libkern/i386/_OSByteOrder.h mach/i386/_structs.h)
         fi
     else
         SYSHEADERS=(secure/_common.h secure/_stdio.h secure/_string.h stdint.h stdio.h errno.h string.h strings.h alloca.h stdlib.h unistd.h time.h dlfcn.h limits.h _types.h _structs.h Availability.h AvailabilityMacros.h AvailabilityInternal.h vproc.h fcntl.h pthread.h pthread_impl.h sched.h sys/select.h sys/unistd.h sys/wait.h sys/errno.h sys/types.h sys/syslimits.h sys/_types.h sys/_endian.h sys/cdefs.h sys/appleapiopts.h sys/_structs.h sys/_symbol_aliasing.h sys/_posix_availability.h sys/signal.h sys/resource.h sys/stat.h sys/_select.h sys/fcntl.h machine/types.h machine/endian.h machine/signal.h machine/limits.h machine/_structs.h                   machine/_types.h  arm/types.h  arm/_types.h  arm/endian.h  arm/limits.h  arm/_limits.h  arm/_structs.h  arm/signal.h libkern/_OSByteOrder.h libkern/arm/OSByteOrder.h   mach/arm/_structs.h arm/arch.h)
+        _SDKARCH=arm
     fi
 
     [ -d usr ] && rm -rf usr
@@ -46,10 +49,24 @@ do_kernel_extract() {
     DST_INCLUDE_FOLDERS=(usr/include $_TARGET/sys-include)
 
     for SYSHDR in ${SYSHEADERS[@]}; do
-        for DST_INCLUDE in ${DST_INCLUDE_FOLDERS}; do
+        for DST_INCLUDE in ${DST_INCLUDE_FOLDERS[@]}; do
             [ ! -d $DST_INCLUDE/$(dirname $SYSHDR) ] && mkdir -p $DST_INCLUDE/$(dirname $SYSHDR)
             cp -R -p $_SRC/usr/include/$SYSHDR $DST_INCLUDE/$(dirname $SYSHDR)
         done
+    done
+
+    # Since libstdc++ doesn't get built, we need to get the headers from an existing SDK.
+    # May as well do that here, even though it's not needed for the build.
+    CT_Pushd "${_SRC}/usr/include/c++"
+    CXX_INCLUDES=$(find . -mindepth 2 -maxdepth 2) # -regex '.*/\([0-9]\.\)*[0-9]')
+    CT_Popd
+
+    for CXX_INCLUDE in $CXX_INCLUDES; do
+        DSTDIR=usr/include/c++/$(echo "${CXX_INCLUDE}" | sed -r -e "s/darwin[0-9]*/darwin${CT_DARWIN_VERSION}/g")
+        CT_DoExecLog ALL \
+        mkdir -p $(dirname "${DSTDIR}")
+        CT_DoExecLog ALL \
+        cp -rf "${_SRC}/usr/include/c++/${CXX_INCLUDE}" "${DSTDIR}"
     done
 
     # libs needed:
