f960e3e84248ca27ec5efd3ef2dd7d17b107150e
Ray Donnelly <mingw.android@gmail.com>
2017-05-05 00:55:02 +0100

    gdb: Disable --with-guile
    .. otherwise the system guile is picked up and that currently fails:
    
    .../.build/src/gdb-7.12.1/gdb/guile/scm-ports.c:1097:41:
      error: invalid use of incomplete type 'scm_t_port {aka struct scm_t_port}'

--- scripts/build/debug/300-gdb.sh
+++ scripts/build/debug/300-gdb.sh
@@ -155,6 +155,7 @@ do_debug_gdb_build() {
             --prefix="${CT_PREFIX_DIR}"                 \
             --with-build-sysroot="${CT_SYSROOT_DIR}"    \
             --with-sysroot="${CT_SYSROOT_DIR}"          \
+            --with-guile=no                             \
             --disable-werror                            \
             "${cross_extra_config[@]}"                  \
             "${CT_GDB_CROSS_EXTRA_CONFIG_ARRAY[@]}"
@@ -285,6 +286,7 @@ do_debug_gdb_build() {
             --without-x                                 \
             --disable-sim                               \
             --disable-werror                            \
+            --with-guile=no                             \
             --without-included-gettext                  \
             --without-develop                           \
             "${native_extra_config[@]}"
